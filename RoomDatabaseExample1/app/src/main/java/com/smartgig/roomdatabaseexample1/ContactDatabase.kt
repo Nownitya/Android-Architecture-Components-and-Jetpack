878907091c6da4ab83cd765da0bcb21df0d7ddc8
Nownitya Sharma <nownitya@gmail.com>
2023-02-13 15:29:29 +0530

    Initial Commit

--- RoomDatabaseExample1/app/src/main/java/com/smartgig/roomdatabaseexample1/ContactDatabase.kt
+++ RoomDatabaseExample1/app/src/main/java/com/smartgig/roomdatabaseexample1/ContactDatabase.kt
@@ -1,11 +1,49 @@
 package com.smartgig.roomdatabaseexample1
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Contact::class], version = 1, exportSchema = true)
 abstract class ContactDatabase : RoomDatabase() {
     abstract fun contactDAO(): ContactDAO
 
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ContactDatabase? = null
+
+//        fun getDatabase(context: Context): ContactDatabase {
+//            if (INSTANCE == null) {
+//                // Only one Instance will be created
+//                synchronized(this) {
+//                    INSTANCE = Room.databaseBuilder(
+//                        context.applicationContext,
+//                        ContactDatabase::class.java, "contactDB"
+//                    ).build()
+//                }
+//
+//            }
+//            return INSTANCE!!
+//        }
+
+        fun getDatabase(context: Context): ContactDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+
+            }
+        }
+
+        private fun buildDatabase(context: Context): ContactDatabase {
+            return Room.databaseBuilder(
+                context,
+                ContactDatabase::class.java,
+                "contactDB"
+            ).build()
+
+
+        }
+    }
 
 }
--- RoomDatabaseExample1/app/src/main/java/com/smartgig/roomdatabaseexample1/MainActivity.kt
+++ RoomDatabaseExample1/app/src/main/java/com/smartgig/roomdatabaseexample1/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.smartgig.roomdatabaseexample1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var database: ContactDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        database = ContactDatabase.getDatabase(this)
+//        val database2 = ContactDatabase.getDatabase(this)
+
+//        database = Room.databaseBuilder(
+//            applicationContext,
+//            ContactDatabase::class.java, "contactDB"
+//        ).build()
+
+        GlobalScope.launch {
+            database.contactDAO().insertContact(Contact(0, "Rohan", "9494949494"))
+        }
+
+    }
+
+    fun getData(view: View) {
+//        database.contactDAO().getContact().observe(this, Observer {
+//            Log.d("CheckData",it.toString())
+//        })
+
+        database.contactDAO().getContact().observe(this) {
+            Log.d("CheckData", it.toString())
+        }
+
     }
 }
